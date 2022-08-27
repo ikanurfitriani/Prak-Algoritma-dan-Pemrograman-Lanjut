d704c26d515155c2502c1a2c17edd6c4976bd6c9
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-08-27 14:22:25 +0700

    Delete Fungsi1.java

--- P9/Fungsi1.java
+++
@@ -1,9 +0,0 @@
-public class Fungsi1{
-	public static int jumlah(int a){
-		return a;
-	}
-	public static void main(String args[]){
-		System.out.println("Hasil pemanggilan methode ");
-		System.out.println(jumlah(5));
-	}
-}
