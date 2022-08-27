7fe264207d85dba2fff683d545274096abbbb4e4
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-08-27 14:01:24 +0700

    Delete Array1_2.java

--- P3/Array1_2.java
+++
@@ -1,9 +0,0 @@
-public class Array1_2 {
-	public static void main(String[] args) {
-		//array 1 dimensi
-		String[] nama = {"Jono","Joni","Jini","Jeni","Juni"};
-		//menampilkan array menggunakan foreach
-		for (String name:nama)
-		System.out.println(name);
-	}
-}
