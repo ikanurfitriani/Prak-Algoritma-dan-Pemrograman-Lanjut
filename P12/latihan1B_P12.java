773e12266c1f774fde481669f9bf1d6af3bbe701
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:49:10 +0700

    Delete P12 directory

--- P12/latihan1B_P12.java
+++
@@ -1,8 +0,0 @@
-public class latihan1B_P12 {
-	public static void main (String args[]) {
-        int[] Angka = {1, 2, 3, 4, 5, 6, 7, 8};
-        for (int i=0; i<Angka.length; i++){
-            System.out.println(Angka[i]);
-        }
-    }
- }
