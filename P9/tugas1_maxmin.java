a0705001bd15f7f263b49f170e4b7e6992999c42
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 16:50:48 +0700

    Delete P9 directory

--- P9/tugas1_maxmin.java
+++
@@ -1,7 +0,0 @@
-public class tugas1_maxmin {
-	public static void main(String args[]){
-      	System.out.println("Method bawaan yaitu Method max and min");
-      	System.out.println("Nilai maksimum dari (41, 96) adalah " + Math.max(41, 96));
-      	System.out.println("Nilai minimum dari (1.7, 8.5) adalah " + Math.min(1.7, 8.5));
-   }
-}
